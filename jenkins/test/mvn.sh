bedd4178d50d3faa432d640fb5b33e272ffd216e
shonphand <shonphand@gmail.com>
2019-11-30 14:46:14 +0530

    changed the PWD to WORSPACE path

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,4 +4,5 @@ echo "******************"
 echo "** testing code  **"
 echo "******************"
 
-docker run --rm  -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+WORKSPACE=/home/shon/Documents/Jenkins/jenkins-home/workspace/jenkins-pipeline
+docker run --rm  -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
