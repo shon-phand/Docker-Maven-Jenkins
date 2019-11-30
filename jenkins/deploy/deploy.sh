708172e43764ab6907a7564dcf61c6549e9562c0
shonphand <shonphand@gmail.com>
2019-11-30 15:15:11 +0530

    changed the hostname

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -2,6 +2,6 @@ echo "maven-project" > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth    
 
-scp -i /opt/prod /tmp/.auth prod-user@master:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@master:~/maven/
-ssh -i /opt/prod prod-user@master "~/maven/publish.sh"
+scp -i /opt/prod /tmp/.auth prod-user@192.168.33.10:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@192.168.33.10:~/maven/
+ssh -i /opt/prod prod-user@192.168.33.10 "~/maven/publish.sh"
