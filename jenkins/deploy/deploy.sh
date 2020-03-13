08cc23b2851ee8b53176e657765b2be427ab9b24
shonphand <shonphand@gmail.com>
2020-03-13 11:56:00 +0530

    added ls

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,6 +1,7 @@
 echo "maven-project" > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
-echo $PASS >> /tmp/.auth    
+echo $PASS >> /tmp/.auth  
+ls -la /opt/
 
 scp -i /opt/prod /tmp/.auth prod-user@192.168.33.10:/tmp/.auth
 scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@192.168.33.10:~/maven/
